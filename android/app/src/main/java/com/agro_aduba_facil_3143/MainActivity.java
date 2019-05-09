c85f76052c10624c9b18df1957d6bbdd8367930d
Crowdbotics <team@crowdbotics.com>
2019-05-09 21:47:21 +0000

    initial commit

---
+++ android/app/src/main/java/com/agro_aduba_facil_3143/MainActivity.java
@@ -0,0 +1,15 @@
+package com.agro_aduba_facil_3143;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "agro_aduba_facil_3143";
+    }
+}
